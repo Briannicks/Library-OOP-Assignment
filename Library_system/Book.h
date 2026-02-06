9910cbaad249878dbd64a530a6ff7c47427b9ff3
Briannicks <your-email@example.com>
2026-02-06 12:13:32 +0300

    .

--- Library_system/Book.h
+++ Library_system/Book.h
@@ -1,13 +1,21 @@
 #ifndef BOOK_H
 #define BOOK_H
-#include <string>
+#include <string> // Must include this
 
 class Book {
-public:
-    std::string title, author, isbn;
+private:
+    std::string title; // Must use std::
+    std::string author;
+    std::string isbn;
     bool isAvailable;
 
+public:
     Book(std::string t, std::string a, std::string i) 
         : title(t), author(a), isbn(i), isAvailable(true) {}
+
+    std::string getTitle() const { return title; }
+    std::string getIsbn() const { return isbn; }
+    bool checkAvailability() const { return isAvailable; }
+    void setAvailability(bool status) { isAvailable = status; }
 };
 #endif
